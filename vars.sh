761c0c5fcd811c09fe6acf44e190557834660b47
Brent Walther <brentwalther@gmail.com>
2021-01-06 19:47:29 -0600

    Make vars.sh explanation comment better.

--- vars.sh
+++ vars.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# This should point to a private copy of vars.sh which contains paths
-# to your ledger executable and files. For inspiration, copy the example
-# vars.sh somewhere.
-source /absolute/path/to/vars.sh
+# This file exists solely as a symlink to your actual (private) `vars.sh`
+# file. If you don't have a `vars.sh`, copy the one in `./examples` to
+# some other directory and reference it here. If there is a better way
+# to do this, please let me know.
+source /some/other/path/to/vars.sh
+
